4394eddf250eefb86b0e32fb555a5bdfac36daaf
PyXiion <me@pyxiion.ru>
2026-06-27 13:29:39 +0300

    Forgot server mixin

---
+++ common/src/main/java/ru/pyxiion/ignis/mixins/MinecraftServerMixin.java
@@ -0,0 +1,26 @@
+package ru.pyxiion.ignis.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.pyxiion.ignis.PxIgnis;
+
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @Inject(method = "reloadResources", at = @At("TAIL"))
+    private void reloadRes(Collection<String> dataPacks, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
+        try {
+            PxIgnis.instance.runtime.reload();
+        } catch (Throwable t) {
+            PxIgnis.logger.error("PxIgnis script reload failed", t);
+            try {
+                PxIgnis.instance.runtime.getEventManager().fire("init");
+            } catch (Throwable ignored) {}
+        }
+    }
+}
